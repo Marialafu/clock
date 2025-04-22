ba0d7fa018c053a86292cf41ece4c49f8a42b6cf
Marialafu <lafuentelhmaria@gmail.com>
2025-04-22 21:41:20 +0200

    updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,13 @@
 const hoursElement = document.getElementById('hours');
 const daysElement = document.getElementById('days');
+const rootStyles = document.documentElement.style
 
 const secondsHandElement = document.getElementById('seconds-hand');
 const minutesHandElement = document.getElementById('minutes-hand');
 const hoursHandElement = document.getElementById('hours-hand');
+
+let secondsDeg = 0;
+let minutesDeg = 0;
 
 const weekDaysList = [
   'Domingo',
@@ -34,13 +38,42 @@ const defineDigitalClock = () => {
   let day = weekDaysList[date.getDay()];
   let month = monthsList[date.getMonth()];
   let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
 
+
+  
+ //DIGITAL CLOCK
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
 
   hoursElement.textContent = `${date.getHours()} : ${minutes}`;
   daysElement.textContent = `${day} ${date.getDate()} ${month} ${date.getFullYear()}`;
-};
 
+  //ANALOGIC CLOCK
+  secondsDeg += 6
+
+  if (secondsDeg === 360){
+    secondsDeg = 0
+    secondsDeg += 6
+  }
+  rootStyles.setProperty('--deg-seconds', `${secondsDeg}deg`)
+
+
+};
 setInterval(defineDigitalClock, 1000);
+
+const defineHours = () => {
+  minutesDeg += 30
+
+  if (minutesDeg === 360){
+    minutesDeg = 0
+    minutesDeg += 30
+  }
+  rootStyles.setProperty('--deg-minutes', `${minutesDeg}deg`)
+
+}
+setInterval(defineHours, 60000);
+
+//30º por hora
+//0.5º por minuto
