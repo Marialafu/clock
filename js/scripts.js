e02f34402ba15949762bedf762a3bbdeff150fd5
Marialafu <lafuentelhmaria@gmail.com>
2025-04-23 10:04:38 +0200

    updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 const hoursElement = document.getElementById('hours');
 const daysElement = document.getElementById('days');
-const rootStyles = document.documentElement.style
+const rootStyles = document.documentElement.style;
 
 const secondsHandElement = document.getElementById('seconds-hand');
 const minutesHandElement = document.getElementById('minutes-hand');
@@ -36,39 +36,24 @@ const defineDigitalClock = () => {
   let month = monthsList[date.getMonth()];
   let minutes = date.getMinutes();
   let seconds = date.getSeconds();
+  let hours = date.getHours();
 
-
-  
- //DIGITAL CLOCK
+  //DIGITAL CLOCK
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
 
-  hoursElement.textContent = `${date.getHours()} : ${minutes}`;
+  hoursElement.textContent = `${String(hours).padStart(2, '0')} : ${minutes}`;
   daysElement.textContent = `${day} ${date.getDate()} ${month} ${date.getFullYear()}`;
 
   //ANALOGIC CLOCK
   let secondsDeg = seconds * 6;
-  let minutesDeg = minutes * 0.5;
+  let minutesDeg = minutes * 6;
   let hoursDeg = hours * 30;
 
-  if (secondsDeg === 360){
-    secondsDeg = 0 * 6
-  }
-  if (minutesDeg === 360){
-    minutesDeg = 0 * 0.5
-  }
-  if (hoursDeg === 360){
-    hoursDeg = 0 * 30
-  }
-
-  rootStyles.setProperty('--deg-seconds', `${secondsDeg}deg`)
-  rootStyles.setProperty('--deg-minutes', `${minutesDeg}deg`)
-  rootStyles.setProperty('--deg-hours', `${hoursDeg}deg`)
-  
-  
-  
-
+  rootStyles.setProperty('--deg-seconds', `${secondsDeg}deg`);
+  rootStyles.setProperty('--deg-minutes', `${minutesDeg}deg`);
+  rootStyles.setProperty('--deg-hours', `${hoursDeg}deg`);
 };
 setInterval(defineDigitalClock, 1000);
 
